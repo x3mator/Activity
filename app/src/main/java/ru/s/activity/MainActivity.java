b5b6cf2e5fc2a1ccfae0114f50a06d113766e98e
Anton <x3mator@gmail.com>
2020-01-13 17:06:13 +0200

    Добавил PopUP

--- app/src/main/java/ru/s/activity/MainActivity.java
+++ app/src/main/java/ru/s/activity/MainActivity.java
@@ -2,13 +2,20 @@ package ru.s.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.EditText;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.PopupMenu.OnMenuItemClickListener;
+import android.app.Activity;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMenuItemClickListener {
 
     static private final int CHOOSE_VIBOR = 0;
     public final static String VIBOR = "VIBOR";
@@ -17,6 +24,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_popup)
+                //Следим за нажатиями по кнопке:
+                .setOnClickListener(new OnClickListener() {
+
+                    //Обрабатываем нажатие кнопки Button:
+                    @Override
+                    public void onClick(View view) {
+                        //Вызываем popup меню, заполняем его с файла popup.xml и настраиваем
+                        //слушатель нажатий по пунктам OnMenuItemClickListener:
+                        PopupMenu popup_menu = new PopupMenu(MainActivity.this, view);
+                        popup_menu.setOnMenuItemClickListener(MainActivity.this);
+                        popup_menu.inflate(R.menu.popup);
+                        popup_menu.show();                    }
+                });
     }
 
     public void onClick(View view) {
@@ -48,5 +70,24 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+
+
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(this, "PopUP 1", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this, "PopUP 2", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(this, "PopUP 3", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return true;
+    }
+
+
 }
 
--- app/src/main/java/ru/s/activity/MainActivityVibor.java
+++ app/src/main/java/ru/s/activity/MainActivityVibor.java
@@ -3,6 +3,7 @@ package ru.s.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,7 +12,10 @@ public class MainActivityVibor extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main_vibor);
+
+        // setTitle("");
     }
 
     public void onRadioClick(View view) {
